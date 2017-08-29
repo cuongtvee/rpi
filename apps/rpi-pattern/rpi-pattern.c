0e6aa9a3254ae3970d7b1676998766509cc7a729
kupro <cuongtv.ee@gmail.com>
2017-08-29 15:16:11 +0700

    Up rpi camera source

---
+++ apps/rpi-pattern/rpi-pattern.c
@@ -0,0 +1,110 @@
+/*
+-- Company: Trenz Electronic GmbH
+-- Engineer: Oleksandr Kiyenko
+ */
+#include <stdio.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#define CAMERA_IIC_ADDRESS      0x36
+
+#define CS_CMMN_CHIP_ID_H       0x300A
+#define CS_CMMN_CHIP_ID_L       0x300B
+
+unsigned char i2c_reg_read(int dev_file, unsigned char dev_addr, unsigned short reg_addr)
+{
+    unsigned char inbuf[2];
+    unsigned char outbuf[2];
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[2];
+
+    /*
+    * In order to read a register, we first do a "dummy write" by writing
+    * 0 bytes to the register we want to read from.  This is similar to
+    * the packet in set_i2c_register, except it's 1 byte rather than 2.
+    */
+    outbuf[0] = reg_addr >> 8;
+    outbuf[1] = reg_addr & 0xFF;
+    messages[0].addr  = dev_addr;
+    messages[0].flags = 0;
+    messages[0].len   = sizeof(outbuf);
+    messages[0].buf   = &outbuf;
+    /* The data will get returned in this structure */
+    messages[1].addr  = dev_addr;
+    messages[1].flags = I2C_M_RD/* | I2C_M_NOSTART*/;
+    messages[1].len   = sizeof(inbuf);
+    messages[1].buf   = inbuf;
+
+    /* Send the request to the kernel and get the result back */
+    packets.msgs      = messages;
+    packets.nmsgs    = 2;
+    if(ioctl(dev_file, I2C_RDWR, &packets) < 0) {
+        perror("Unable to send data");
+        return 1;
+    }
+
+    return inbuf[0];
+}
+
+int i2c_reg_write(int dev_file, unsigned char dev_addr, unsigned short reg_addr, unsigned char reg_data)
+{
+    unsigned char outbuf[3];
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[1];
+
+    messages[0].addr  = dev_addr;
+    messages[0].flags = 0;
+    messages[0].len   = sizeof(outbuf);
+    messages[0].buf   = &outbuf;
+    /* The first byte indicates which register we'll write */
+    outbuf[0] = reg_addr >> 8;
+    outbuf[1] = reg_addr & 0xFF;
+    outbuf[2] = reg_data;
+    /* Transfer the i2c packets to the kernel and verify it worked */
+    packets.msgs  = messages;
+    packets.nmsgs = 1;
+    if(ioctl(dev_file, I2C_RDWR, &packets) < 0) {
+        perror("Unable to send data");
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int i2c_file;
+
+    printf("Raspberry Pi Camera Mode\n");
+    if(argc < 2){
+        printf("%s /dev/i2c-X [mode]\n",argv[0]);
+        exit(1);
+    }
+
+    if ((i2c_file = open(argv[1], O_RDWR)) < 0) {
+        perror("Unable to open i2c control file");
+        exit(1);
+    }
+
+    if((i2c_reg_read(i2c_file, CAMERA_IIC_ADDRESS, CS_CMMN_CHIP_ID_H) != 0x56) || (i2c_reg_read(i2c_file, CAMERA_IIC_ADDRESS, CS_CMMN_CHIP_ID_L) != 0x47)){
+        perror("Camera not found");
+        exit(1);
+    }
+
+    if(i2c_reg_read(i2c_file, CAMERA_IIC_ADDRESS, 0x503D)){
+        printf("Switch to camera mode.\n");
+        i2c_reg_write(i2c_file, CAMERA_IIC_ADDRESS, 0x503D, 0x00);
+    }
+    else{
+        printf("Switch to pattern mode.\n");
+        i2c_reg_write(i2c_file, CAMERA_IIC_ADDRESS, 0x503D, 0x80);
+    }
+    close(i2c_file);
+	return 0;
+}
+
+
